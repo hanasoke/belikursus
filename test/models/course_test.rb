2790ab1e6e74f480ec441b4236dd9d1de910db97
hanasoke@gmail.com <hanasoke@gmail.com>
2024-12-09 21:39:58 +0700

    BK-5: add test for course model

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -5,5 +5,19 @@ class CourseTest < ActiveSupport::TestCase
     course = Course.new
     assert course.invalid?
     assert course.errors[:title].any?
-    assert course.errors[:description]
+    assert course.errors[:description].any?
+    assert course.errors[:image_url].any?
+    assert course.errors[:price].any?
+  end
+
+  test "price must be positive" do
+    course = Course.new(title: "Testing model", description: "model desc", image_url: "model.png")
+
+    course.price = -1
+    assert course.invalid?
+    assert_equal(["must be greater than or equal to 1"], course.errors[:price])
+
+    course.price = 1
+    assert course.valid?
+  end
 end
